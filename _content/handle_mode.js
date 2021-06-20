2d7eeb0dcb0a3e41390f02d21b7632dc643d4ab8
nas <nas.silver.bullet@gmail.com>
2021-06-20 19:24:18 +0900

    refactor js

--- _content/handle_mode.js
+++ _content/handle_mode.js
@@ -1,21 +1,3 @@
-const init = (checkbox) => {
-    checkbox.addEventListener("change", function() {
-        if (checkbox.checked) {
-            setDark(checkbox);
-            return;
-        }
-
-        setNormal(checkbox);
-    });
-
-    if (sessionStorage.getItem("mode") == "dark") {
-        setDark(checkbox);
-        return;
-    }
-
-    setNormal(checkbox);
-};
-
 const setDark = (checkbox) => {
     document.body.classList.add("dark-mode");
     checkbox.checked = true;
@@ -26,6 +8,24 @@ const setNormal = (checkbox) => {
     document.body.classList.remove("dark-mode");
     checkbox.checked = false;
     sessionStorage.setItem("mode", "light");
+};
+
+const init = (checkbox) => {
+    checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+            setDark(checkbox);
+            return;
+        }
+
+        setNormal(checkbox);
+    });
+
+    if (sessionStorage.getItem("mode") === "dark") {
+        setDark(checkbox);
+        return;
+    }
+
+    setNormal(checkbox);
 };
 
 init(document.getElementById("handle-mode"));
